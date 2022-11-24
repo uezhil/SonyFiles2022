c270e7bcd2c28535b9e016bae4e53f3340ea9317
Ezhil <uezhil@gmail.com>
2022-11-24 16:22:39 +0530

    Add files via upload

---
+++ JsBasics.js
@@ -0,0 +1,11 @@
+//Display Methods
+alert("Alert MEthod");
+document.write("Document Write method");
+confirm("Do you really want to close this?");
+console.log("COnsole log method");
+
+var first_name = "Maya",
+  age = 0;
+first_name = prompt("What's your name");
+age = prompt("Enter your age");
+alert("Latest name is " + first_name + " " + typeof age);
