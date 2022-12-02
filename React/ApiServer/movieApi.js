a294e96307d4264fd66e72a53e601819711d9b6b
Ezhil <uezhil@gmail.com>
2022-12-02 11:30:52 +0530

    Update movieApi.js

--- React/ApiServer/movieApi.js
+++ React/ApiServer/movieApi.js
@@ -5,3 +5,16 @@ export function getMovies(){
     return fetch(url).then(ResponseChk).catch(ErrorChk);
 }
 
+export function getMoviebySlug(slug) {
+  return fetch(url + "?slug=" + slug)
+    .then((response) => {
+      if (!response.ok)
+        throw new Error("Movie by slug cannot be retrieved ..error");
+      return response.json().then((movies) => {
+        if (movies.length !== 1)
+          throw new Error("Cannot find the movie.." + slug);
+        return movies[0];
+      });
+    })
+    .catch(ErrorChk);
+}
